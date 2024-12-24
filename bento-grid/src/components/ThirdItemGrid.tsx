6f3e6fd1fef3c46f9bb4dfb30bb6ad750cc037d8
Nina <nina.micleusanu93@gmail.com>
2024-12-24 19:21:47 +0100

    update layout for responsiveness and enhance ThirdItemGrid styling (wip)

--- bento-grid/src/components/ThirdItemGrid.tsx
+++ bento-grid/src/components/ThirdItemGrid.tsx
@@ -1,10 +1,22 @@
 const ThirdItemGrid = () => {
   return (
-    <div className="bg-purple-100 rounded-md">
-      Schedule to social media.
-      <img src="images/illustration-schedule-posts.webp" alt="Schedule posts" />
-      Optimize post timings to publish content at the perfect time for your
-      audience.
+    <div className="bg-purple-100 rounded-md flex flex-col items-center h-full py-8">
+      <div className="flex flex-col gap-2 pl-6">
+        <div className="text-2xl text-start leading-6 pl-2">
+          Schedule to social media.
+        </div>
+        <div className="overflow-hidden">
+          <img
+            className="ml-2 h-full"
+            src="images/illustration-schedule-posts.webp"
+            alt="Schedule posts"
+          />
+        </div>
+        <div className="pl-2 text-start text-md leading-4">
+          Optimize post timings to publish content at the perfect time for your
+          audience.
+        </div>
+      </div>
     </div>
   );
 };
