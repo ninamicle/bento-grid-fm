09a7a9f36dc99d97fa5e00ad6c57aaf1caeec25f
Nina <nina.micleusanu93@gmail.com>
2024-12-24 19:43:03 +0100

    refactor FifthItemGrid component for improved layout and structure

--- bento-grid/src/components/FifthItemGrid.tsx
+++ bento-grid/src/components/FifthItemGrid.tsx
@@ -1,13 +1,22 @@
 const FifthItemGrid = () => {
   return (
-    <div className="bg-white rounded-md">
-      <img
-        src="images/illustration-multiple-platforms.webp"
-        alt="Multiple platforms"
-      />
-      Manage multiple accounts and platforms.
+    <div className="bg-white rounded-md text-2xl h-full flex flex-col items-center gap-y-5 justify-center overflow-hidden">
+      <ContentImage />
+      <div className="text-start leading-6 font-dm-medium text-black px-4">
+        Manage multiple accounts and platforms.
+      </div>
     </div>
   );
 };
+
+const ContentImage = () => (
+  <div className="w-64 pl-1">
+    <img
+      className="ml-10"
+      src="images/illustration-multiple-platforms.webp"
+      alt="Multiple platforms"
+    />
+  </div>
+);
 
 export default FifthItemGrid;
