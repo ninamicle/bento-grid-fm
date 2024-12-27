dc38cbf3c680832d2c5192d0ea3f1e78c484bd4e
Nina <nina.micleusanu93@gmail.com>
2024-12-27 11:09:00 +0100

    refactor SeventhItemGrid component for improved layout and styling

--- bento-grid/src/components/SeventhItemGrid.tsx
+++ bento-grid/src/components/SeventhItemGrid.tsx
@@ -1,11 +1,15 @@
 const SeventhItemGrid = () => {
   return (
-    <div className="bg-white rounded-md">
-      {">"}56% faster audience growth
-      <img
-        src="images/illustration-audience-growth.webp"
-        alt="Audience growth"
-      />
+    <div className="bg-white rounded-md h-full text-start p-4 py-6">
+      <div className="text-7xl">{">"}56% </div>
+      <div className="py-2 text-xl">faster audience growth</div>
+      <div className="flex justify-start items-end mr-8 h-1/2">
+        <img
+          className="w-5/6 mb-4"
+          src="images/illustration-audience-growth.webp"
+          alt="Audience growth"
+        />
+      </div>
     </div>
   );
 };
